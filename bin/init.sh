871069a022a70af5f682a6a34430d51b1b23a129
ecwpz91 <ecwpz91@users.noreply.github.com>
2016-11-23 17:12:59 -0500

    Check timestamp before wget operation

--- bin/init.sh
+++ bin/init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Downloading CloudForms database backups locally for the environment in the MBU lab"
-wget http://10.9.62.89/dumps/vmdb_production_latest.dump
+wget -N http://10.9.62.89/dumps/vmdb_production_latest.dump
 
 echo "Downloading MBU region ID for database configuration"
-wget http://10.9.62.89/dumps/REGION
+wget -N http://10.9.62.89/dumps/REGION
 
 echo "Downloading v2_key for database password"
-wget http://10.9.62.89/dumps/v2_key
+wget -N http://10.9.62.89/dumps/v2_key
 
 echo "Creating image from Dockerfile"
 docker build -t cfme-docker/cfme4:1.1 .
