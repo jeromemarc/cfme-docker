3667fddb42c61600036f8f44f75a018ae840bd29
ecwpz91 <ecwpz91@users.noreply.github.com>
2016-11-23 18:05:37 -0500

    Added 'latest' & removed absolute path

--- restore_db.sh
+++ restore_db.sh
@@ -2,8 +2,8 @@
 DBASE="vmdb_production"
 
 echo "Stop evmserverd"
-/usr/bin/systemctl stop evmserverd
-/usr/bin/systemctl stop httpd
+systemctl stop evmserverd
+systemctl stop httpd
 
 echo "Set Region ID"
 mv -f /tmp/REGION /var/www/miq/vmdb/
@@ -12,16 +12,16 @@ echo "Update v2_key"
 mv -f /tmp/v2_key /var/www/miq/vmdb/certs/
 
 echo "Drop database"
-/opt/rh/rh-postgresql94/root/usr/bin/dropdb --no-password -U root $DBASE
+dropdb --no-password -U root $DBASE
 
 echo "Create database"
-/opt/rh/rh-postgresql94/root/usr/bin/createdb --no-password -U root $DBASE
+createdb --no-password -U root $DBASE
 
 echo "Restore database"
-/opt/rh/rh-postgresql94/root/usr/bin/pg_restore --no-password -U root -d $DBASE $DBASE.dump
+pg_restore --no-password -U root -d $DBASE $DBASE_latest.dump
 
 echo "Start evmserverd"
-/usr/bin/systemctl start evmserverd
+systemctl start evmserverd
 
 # echo "Clean temp directory"
 # rm -rf /tmp/*
