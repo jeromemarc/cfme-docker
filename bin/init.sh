4ee88cc084332cc2c4449f3d7e22698fa3c898fc
ecwpz91 <ecwpz91@users.noreply.github.com>
2016-12-06 16:53:15 -0500

    Modified to push to Docker Hub

--- bin/init.sh
+++ bin/init.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
-echo "Downloading CloudForms database backups locally for the environment in the MBU lab"
+DOCKER_ID=ecwpz91
+
+echo "Downloading database backup..."
 wget -N http://10.9.62.89/dumps/vmdb_production_latest.dump
 
-echo "Downloading MBU region ID for database configuration"
+echo "Downloading region ID..."
 wget -N http://10.9.62.89/dumps/REGION
 
-echo "Downloading v2_key for database password"
+echo "Downloading v2_key..."
 wget -N http://10.9.62.89/dumps/v2_key
 
-echo "Creating image from Dockerfile"
-docker build -t cfme-docker/cfme4:1.1 .
+echo "Creating docker image."
+docker build -t "$DOCKER_ID"/cfme4-demo:latest .
 
-# echo "Starting docker container"
-# docker run --privileged -di -p 80:80 -p 443:443 --name cfme4 cfme-docker/cfme4:1.1
-#
-# echo "Restoring demo database"
-# docker exec cfme4 /tmp/restore_db.sh
-#
-# echo "Committing container’s file changes or settings into a new image"
-# docker commit $(docker ps -aq -f name=cfme4) ecwpz91/cfme4-demo:latest
-#
-# echo "Push docker iamge to Docker Hub"
-# docker push ecwpz91/cfme4-demo
+echo "Pushing image to Docker Hub"
+docker push "$DOCKER_ID"/cfme4-demo
