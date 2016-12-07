f1765b3660a402d656c50418d6d36ea8777c0603
ecwpz91 <ecwpz91@users.noreply.github.com>
2016-12-06 19:51:04 -0500

    Finalized

--- restore_db.sh
+++ restore_db.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 DBASE="vmdb_production"
 
-echo "Stop evmserverd"
-systemctl stop evmserverd
-
 echo "Drop database"
 dropdb --no-password -U root $DBASE
 
@@ -19,11 +16,8 @@ mv -f /tmp/REGION /var/www/miq/vmdb/
 echo "Update v2_key"
 mv -f /tmp/v2_key /var/www/miq/vmdb/certs/
 
-echo "Start evmserverd"
-systemctl restart evmserverd
-
-echo "Clean temp directory"
-rm -rf /tmp/*
+echo "Remove database dump file"
+rm -rf "/tmp/"$DBASE"_latest.dump"
 
 echo "Set admin credentials"
 /var/www/miq/vmdb/script/rails r "User.find_by_userid('admin').update_attributes(:password => 'smartvm')"
