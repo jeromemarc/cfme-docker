4ce3bce465cd14079b46091a9c0b5c064a28d184
ecwpz91 <ecwpz91@users.noreply.github.com>
2017-01-02 13:36:30 -0500

    Removed REGION and v2_key pull

--- bin/init.sh
+++ bin/init.sh
@@ -2,13 +2,7 @@
 DOCKER_ID=ecwpz91
 
 echo "Downloading database backup..."
-wget -N http://10.9.62.89/dumps/vmdb_production_latest.dump
-
-echo "Downloading region ID..."
-wget -N http://10.9.62.89/dumps/REGION
-
-echo "Downloading v2_key..."
-wget -N http://10.9.62.89/dumps/v2_key
+wget -N  http://10.9.62.89/dumps/42/vmdb_production_latest.dump
 
 echo "Creating docker image."
 docker build -t "$DOCKER_ID"/cfme4-demo:latest .
