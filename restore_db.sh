a2dd6a2442d914c2e81b93455f5830991facd963
ecwpz91 <ecwpz91@users.noreply.github.com>
2017-01-02 13:44:06 -0500

    Added fix auth and removed REGION / v2_key move

--- restore_db.sh
+++ restore_db.sh
@@ -10,14 +10,8 @@ createdb --no-password -U root $DBASE
 echo "Restore database"
 pg_restore --no-password -U root -d $DBASE "/tmp/"$DBASE"_latest.dump"
 
-echo "Set Region ID"
-mv -f /tmp/REGION /var/www/miq/vmdb/
+echo "Fix authentication"
+bundle exec /var/www/miq/vmdb/tools/fix_auth.rb --v2 --invalid bogus
 
-echo "Update v2_key"
-mv -f /tmp/v2_key /var/www/miq/vmdb/certs/
-
-echo "Remove database dump file"
+echo "Remove dump file"
 rm -rf "/tmp/"$DBASE"_latest.dump"
-
-echo "Set admin credentials"
-/var/www/miq/vmdb/script/rails r "User.find_by_userid('admin').update_attributes(:password => 'smartvm')"
